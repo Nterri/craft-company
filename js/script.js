354a8e18d15c1d58992d220146dbfdf385d2b57b
Nterri <99561765+Nterri@users.noreply.github.com>
2022-02-13 13:50:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const iconMenu = document.querySelector('.header__burger');
 const menuBody = document.querySelector('.header__menu');
 const logo = document.querySelector('.header__logo');
+const wrapper = document.querySelector('.wrapper');
 if (iconMenu) {
     iconMenu.addEventListener('click', function (e) {
         document.body.classList.toggle('lock');
@@ -8,11 +9,18 @@ if (iconMenu) {
         menuBody.classList.toggle('active');
         logo.classList.toggle('active');
         logo.classList.toggle('active-reverse');
+        if (document.body.classList.contains('lock')) {
+            wrapper.style.paddingRight = lockPaddingValue;
+        } else {
+            wrapper.style.paddingRight = "0px";
+        }
     });
 };
+const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+console.log(lockPaddingValue);
 
-$(document).ready(function() {
-    $('.answer__item-title').click(function(e) {
+$(document).ready(function () {
+    $('.answer__item-title').click(function (e) {
         if ($('.answer__body').hasClass('one')) {
             $('.answer__item-title').not($(this)).removeClass('active');
             $('.answer__item-text').not($(this).next()).slideUp(300);
